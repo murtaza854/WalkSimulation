351936f2b5dbb041bfaaf67f1ab893d170d1feba
Nimrah Jawed <nj04707@st.habib.edu.pk>
2020-06-19 16:46:11 +0500

    Add files via upload

---
+++ task7.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import math
+import turtle
+
+x, y = 0, 0
+
+steps = 1000
+lst = []
+xlist = []
+fig, ax = plt.subplots()
+ylist = []
+c = 0
+R = 100
+for j in range(100):
+    for i in range(steps + 1):
+        r = random.choice([0, 1])
+        Theta = np.random.uniform(0, 360)
+        Theta = Theta * (math.pi/180)
+        x += r * math.cos(Theta)
+        y += r * math.sin(Theta)
+        xlist.append(x)
+        ylist.append(y)
+        print(int(math.sqrt((x**2 + y**2))))
+        if int(math.sqrt(x**2 + y**2)) >= R:
+            x -= r * math.cos(Theta)
+            y -= r * math.sin(Theta)
+circle1 = plt.Circle((0, 0), 100, fill=False)
+
+ax.add_artist(circle1)
+plt.plot(xlist, ylist)
+plt.xlim(-100, 100)
+plt.xlabel("distance in x axis")
+plt.ylabel("distance in y axis")
+plt.ylim(-100, 100)
+plt.show()
